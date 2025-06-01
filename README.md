7ade63a4200109bce5970642c57521ac07bbe26b
kgs4787 <kks478743@gmail.com>
2025-06-01 18:38:14 +0900

    부동산 웹앱 클론

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
 # vue-practice
 
-## Project setup
-```
-npm install
-```
+Vue 연습용 클론코딩 프로젝트
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## 1. 데이터 바인딩
 
-### Compiles and minifies for production
-```
-npm run build
-```
+js 데이터를 HTML에 넣는 문법 / HTML 속성도 데이터바인딩 가능
 
-### Lints and fixes files
-```
-npm run lint
-```
+**자바스크립트**<br>
+document.getElementById().innerHTML = ??
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+**vue** <br>
+data에 object 자료형으로 저장후 {{ 데이터이름 }} :style="스타일"
+
+**데이터바인딩 하는 이유**<br>
+
+1. HTML에 하드코딩하면 나중에 변경이 어려움
+2. vue의 실시간 자동 렌더링 사용
+
+## 2. 반복문
+
+<태그 v-for="(a,i) in array/object" :key="i"><br>
+:key"" -반복문 쓸떄 필수 -반복문 돌린 요소를 컴퓨터가 구분하기 위해
+
+## 3. 이벤트 핸들러
+
+<태그 v-on> / <태그 @><br>
+@click @mouseover ....
+
+## 4. 조건문
+
+<태그 v-if>
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  아ㅏ녕아헤숑
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="black-bg" v-if="모달창열림">
+    <div class="white-bg">
+      <h4 @click="모달창열림 = false">상세페이지</h4>
+      <p>상세페이지내용</p>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <div v-for="(방이름, i) in products" :key="i">
+    <img :src="방이름.image" alt="Room Image" class="room-img" />
+    <h4 @click="모달창열림 = true">{{ 방이름.title }}</h4>
+    <p>{{ 방이름.price }}원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import data from "./assets/room.js";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      모달창열림: true,
+      신고수: [0, 0, 0],
+      메뉴들: ["Home", "About", "Contact"],
+      products: data,
+    };
   },
+  methods: {
+    incresse(i) {
+      this.신고수[i] += 1;
+    },
+  },
+  components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 300px;
+  margin-top: 40px;
 }
 </style>
